cdd5920e59704003c8f199441ca0a85c92102ae5
KandrewC <andrewkan@D-10-157-118-43.dhcp4.washington.edu>
2016-02-22 18:21:23 -0800

    Finished chart1 Close Issue #4

---
+++ build_chart1.R
@@ -0,0 +1,25 @@
+data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv")
+library(dplyr);
+library(plotly);
+
+
+build_chart1 <- function(data) {
+  xAxis <- list(
+    title = "Proficiency in R"
+  )
+  yAxis <- list(
+    title = "Number of People"
+  )
+  chart1 <- plot_ly(data, 
+          x = What.is.your.familiarity.with..Using.the.R.programming.language,
+          y = What.is.your.programming.experience., 
+          color=What.is.your.familiarity.with..Using.the.R.programming.language,
+         type = "histogram"
+  )  %>% 
+    layout(title = "R Proficiency Before Class", margin = list(l = 100), margin = list(b = 0),
+           xaxis = xAxis, yaxis = yAxis)
+  
+
+  return(chart1)
+}
+
