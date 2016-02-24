fc4b6df65e81316b5a76a1e06659d291a8bbd253
Jill Aneri Shah <shahj@uw.edu>
2016-02-24 00:00:22 -0800

    Fixed chart

--- build_chart2.r
+++ build_chart2.r
@@ -4,11 +4,12 @@ build_chart2 <- function(data){
 
   chart2 <- ploy_ly(data,
                       x = What.operating.system.do.you.typically.use,
-                      y = What.is.your.familiarity.with..Using.the.R.programming.language,
+                      y = What.is.your.familiarity.with.Using.the.R.programming.language,
                       name = "Operating System vs Familiarity with R",
                       type = "bar",
-                      filename = "r-docs/simple-bar"
-                      )
+                      color = What.is.your.familiarity.with.Using.the.R.programming.language
+                      ) %>%
+    layout(title = "Operating System vs Familiarity with R", margin = list(l = 100), margin = list(b = 0))
 
     return(chart1)
   
