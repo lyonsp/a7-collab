5f202f58f33daff2aa6a628de8b3ec9b8dea6e58
Jill Aneri Shah <shahj@uw.edu>
2016-02-23 23:45:17 -0800

    Bar graph

--- build_chart2.r
+++ build_chart2.r
@@ -1,18 +1,13 @@
-#Compares Windows vs. Mac users and their familiarity with R
+#Bar Graph 
 build_chart2 <- function(data){
+
 
   chart2 <- ploy_ly(data,
                       x = What.operating.system.do.you.typically.use,
                       y = What.is.your.familiarity.with..Using.the.R.programming.language,
-                      name = "Windows",
-                      mode = "markers",
-                      marker = list(color = "black"))%>%
-                      add_trace(x = What.operating.system.do.you.typically.use,
-                                 name = "Mac",
-                                 marker = list(color = "blue"))%>%
-                      layout(
-                        title = "Windows vs. Mac and R programming familiarity",
-                        xaxis = list(title = "Windows vs. Mac")               
+                      name = "Operating System vs Familiarity with R",
+                      type = "bar",
+                      filename = "r-docs/simple-bar"
                       )
 
     return(chart1)
